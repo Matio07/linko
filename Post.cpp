c9fbfe6c46dae74fb8cec3469daa0696a3fffd85
mahshidzivariathar-netizen <mahshid.zivariathar@gmail.com>
2026-02-13 23:05:34 +0330

    Update Post.cpp
    Dar version jadid Post.cpp, constructor default be sorat explicit initialize shode (id, likeCount, postTime) ke baes mishe object dar halat default meghdar moshakhas dashte bashe va safety bala bere.
    Dar constructor asli ham cast be (long) baraye time(nullptr) ezafe shode ta type sakht-tar va clear-tar bashe.
    
    Getter haye getContent va getAuthor be jaye return kardan const reference, alan value bar migardanand ke encapsulation behtar mishe.
    
    Dar kol structure class tamiz-tar, standard-tar va az nazar initialization amn-tar shode vali functionality asli taghir bozorgi nakarde ast.

--- Post.cpp
+++ Post.cpp
@@ -2,25 +2,26 @@
 #include <ctime>
 using namespace std;
 
-Post::Post() {}
+Post::Post() : id(0), likeCount(0), postTime(0) {}
 
 Post::Post(int id, const string& content, const string& author)
     : id(id), content(content), author(author), likeCount(0) {
-    postTime = time(nullptr);
+    postTime = (long)time(nullptr);
 }
 
 int Post::getId() const { return id; }
-const string& Post::getContent() const { return content; }
-const string& Post::getAuthor() const { return author; }
+string Post::getContent() const { return content; }
+string Post::getAuthor() const { return author; }
+
 int Post::getLikeCount() const { return likeCount; }
 long Post::getPostTime() const { return postTime; }
+
 const vector<Comment>& Post::getComments() const { return comments; }
 
+void Post::setContent(const string& c) { content = c; }
 void Post::like() { likeCount++; }
 
-void Post::addComment(const Comment& comment) { comments.push_back(comment); }
+void Post::addComment(const Comment& c) { comments.push_back(c); }
 
-void Post::setLikeCount(int likes) { likeCount = likes; }
+void Post::setLikeCount(int c) { likeCount = c; }
 void Post::setPostTime(long t) { postTime = t; }
-
-void Post::setContent(const string& newContent) { content = newContent; }
