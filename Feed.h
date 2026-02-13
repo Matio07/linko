a9f431521898e68526c4131cedfc545f60bfdc6b
Matio07 <matin.aeini01@gmail.com>
2026-02-13 22:54:37 +0330

    Update Feed.h
    tamiz kardan header Feed va rename kardan param constructor

--- Feed.h
+++ Feed.h
@@ -1,15 +1,16 @@
 #ifndef FEED_H
 #define FEED_H
 
-#include "Network.h"
+#include <string>
 #include <vector>
+#include "Network.h"
 
 class Feed {
 private:
     Network* network;
 
 public:
-    Feed(Network* network);
+    Feed(Network* n);
     std::vector<Post> generateFeed(const std::string& username);
 };
 
