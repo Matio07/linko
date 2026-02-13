352035ed1c8c549dafd79ba552d6bb096084948c
Matio07 <matin.aeini01@gmail.com>
2026-02-13 15:12:29 +0330

    Create AuthManager.h
    AuthManager.h ro add kardam.
    Class AuthManager baraye register/login/logout define shod va field currentUser baraye manage kardan login state ezafe shod.

---
+++ AuthManager.h
@@ -0,0 +1,23 @@
+#ifndef AUTHMANAGER_H
+#define AUTHMANAGER_H
+
+#include <string>
+#include "Network.h"
+
+class AuthManager {
+private:
+    Network* network;
+    std::string currentUser;
+
+public:
+    AuthManager(Network* network);
+
+    bool registerUser(const std::string& username, const std::string& password);
+    bool login(const std::string& username, const std::string& password);
+    void logout();
+
+    bool isLoggedIn() const;
+    const std::string& getCurrentUser() const;
+};
+
+#endif
