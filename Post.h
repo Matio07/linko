d65ff565fe8ceababb0d42574e0cd5fb0356a77e
mahshidzivariathar-netizen <mahshid.zivariathar@gmail.com>
2026-02-19 23:39:57 +0330

    Update Post.h

--- Post.h
+++ Post.h
@@ -12,7 +12,12 @@ private:
     std::string author;
     int likeCount;
     long postTime;
+
+    std::string likedBy; // format: ;user1;user2;
+
     std::vector<Comment> comments;
+
+    int countLikesFromString() const;
 
 public:
     Post();
@@ -25,15 +30,20 @@ public:
     int getLikeCount() const;
     long getPostTime() const;
 
+    const std::string& getLikedByString() const;
+
+    bool hasLiked(const std::string& username) const;
+    bool likeBy(const std::string& username);
+
     const std::vector<Comment>& getComments() const;
 
     void setContent(const std::string& c);
     void like();
-
     void addComment(const Comment& c);
 
     void setLikeCount(int c);
     void setPostTime(long t);
+    void setLikedByString(const std::string& s);
 };
 
 #endif
